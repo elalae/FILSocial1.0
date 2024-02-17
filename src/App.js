c938e6ad5e4a16e59f292861e08297b2470a7387
Liferi Alexandra - Elena <33804236+elalae@users.noreply.github.com>
2024-02-17 12:08:41 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import PageRender from './PageRender';
 function App() {
   return (
+    <Router>
+ 
+    <input type="checkbox" id="theme"/>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+
+        <Route exact path="/:page" component={PageRender} />
+        <Route exact path="/:page/:id" component={PageRender} />
+   
+      </div>
     </div>
+    </Router>
   );
 }
 
 export default App;
+
